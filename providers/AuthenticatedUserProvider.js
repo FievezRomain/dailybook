cf0c0ce850e47cab21f119870eb8302e37fb71f2
Romain Fievez <romain.fievezjr@gmail.com>
2024-02-10 13:08:40 +0100

    Ajout de certaines lib + suppression de l'ancien datePicker incompatible android

--- providers/AuthenticatedUserProvider.js
+++ providers/AuthenticatedUserProvider.js
@@ -3,7 +3,7 @@ import React, { useState, createContext } from 'react';
 export const AuthenticatedUserContext = createContext();
 
 export default function AuthenticatedUserProvider ({ children }) {
-  const [user, setUser] = useState({email: "romain"});
+  const [user, setUser] = useState({email: "test"});
 
   return (
     <AuthenticatedUserContext.Provider value={{ user, setUser }}>
