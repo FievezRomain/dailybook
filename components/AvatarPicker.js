212e7a8e7ca25280523f359fbbca9b7126fc0317
Romain Fievez <romain.fievezjr@gmail.com>
2024-07-21 19:54:12 +0200

    Ajout de la possibilité de prendre une photo directement dans l'AvatarPicker

--- components/AvatarPicker.js
+++ components/AvatarPicker.js
@@ -3,9 +3,20 @@ import * as ImagePicker from 'expo-image-picker';
 import Button from './Button';
 import { Text, StyleSheet, View, TouchableOpacity } from "react-native";
 import Variables from './styles/Variables';
+import ModalSubMenuAvatarPickerActions from './Modals/ModalSubMenuAvatarPicker';
+import { useState } from 'react';
 
-const AvatarPicker = ({ setImage, setValue }) => {
+const AvatarPicker = ({ setImage, setValue, backgroundColor=Variables.rouan }) => {
+  const [modalVisibleSubMenu, setModalVisibleSubMenu] = useState(false);
+
   const pickImageAsync = async () => {
+    // Demande la permission d'utiliser la lib photo
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (status !== 'granted') {
+      alert("Désolé, nous avons besoin des permissions d'accès à la librairie photo!");
+      return;
+    }
+
     let result = await ImagePicker.launchImageLibraryAsync({
       allowsEditing: true,
       quality: 1,
@@ -17,31 +28,67 @@ const AvatarPicker = ({ setImage, setValue }) => {
       setValue("image", result.assets[0].uri);
       //setValue("image", result.assets[0].base64);
     }
+
+    setModalVisibleSubMenu(false);
   };
 
+  // Fonction pour ouvrir l'appareil photo
+  const takePhotoAsync = async () => {
+    // Demande la permission d'utiliser l'appareil photo
+    const { status } = await ImagePicker.requestCameraPermissionsAsync();
+    if (status !== 'granted') {
+      alert('Désolé, nous avons besoin des permissions de caméra pour faire cela!');
+      return;
+    }
+
+    let result = await ImagePicker.launchCameraAsync({
+      allowsEditing: true,
+      quality: 1,
+      base64: true
+    });
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+      setValue("image", result.assets[0].uri);
+      // setValue("image", result.assets[0].base64); // Si vous avez besoin de stocker l'image en base64
+    }
+
+    setModalVisibleSubMenu(false);
+  };
+
+  const styles = StyleSheet.create({
+    textInput:{
+      alignSelf: "flex-start",
+      marginBottom: 5,
+      width: "100%",
+    },
+    buttonContainer:{
+      backgroundColor: backgroundColor,
+      borderRadius: 5,
+      padding: 10,
+    }
+  });
+
   return (
-      <TouchableOpacity 
-        style={styles.textInput} 
-        onPress={pickImageAsync} 
-      >
-        <View style={styles.buttonContainer}>
-              <Text>Sélectionner une image</Text>
-        </View>
-      </TouchableOpacity>
+    <>
+      <ModalSubMenuAvatarPickerActions
+          handleCameraPick={takePhotoAsync}
+          handleLibraryPick={pickImageAsync}
+          modalVisible={modalVisibleSubMenu}
+          setModalVisible={setModalVisibleSubMenu}
+      />
+      <View>
+        <TouchableOpacity 
+          style={styles.textInput} 
+          onPress={() => setModalVisibleSubMenu(true)} 
+        >
+          <View style={styles.buttonContainer}>
+            <Text>Sélectionner une image</Text>
+          </View>
+        </TouchableOpacity>
+    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  textInput:{
-    alignSelf: "flex-start",
-    marginBottom: 5,
-    width: "100%",
-  },
-  buttonContainer:{
-    backgroundColor: Variables.rouan,
-    borderRadius: 5,
-    padding: 10,
-  }
-});
 
 export default AvatarPicker;
