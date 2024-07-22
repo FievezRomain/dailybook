d1b2125e0baa893b4acba03d6fac03786415a48e
Romain Fievez <romain.fievezjr@gmail.com>
2024-07-22 12:50:34 +0200

    Modification de la page d'accueil non connecté pour mettre le bon logo et mettre différents éléments UI

--- screens/Home.js
+++ screens/Home.js
@@ -1,28 +1,46 @@
-import { View, Text, StyleSheet, ImageBackground, TouchableOpacity } from "react-native";
-import wallpaper_accueil from "../assets/wallpaper_accueil.jpg";
+import { View, Text, StyleSheet, ImageBackground, TouchableOpacity, Image, Dimensions } from "react-native";
+import wallpaper_accueil from "../assets/wallpaper_login.png";
+import logo from "../assets/logo.png";
 import variables from "../components/styles/Variables";
-import { Image } from "react-native";
+import WavyHeader from "../components/WavyHeader";
 
 const HomeScreen = ({ navigation })=> {
     return (
         <>
           <Image source={wallpaper_accueil} style={styles.image}/>
-            <View style={styles.partieInf}>
-                    <TouchableOpacity onPress={()=>navigation.navigate("Login")} style={styles.button}>
-                        <Text style={styles.text}>Vivre l'aventure</Text>
-                    </TouchableOpacity>
+          <WavyHeader
+            customBgColor={variables.rouan}
+            customHeight={140}
+            customTop={-80}
+            customWavePattern={"M0,64L60,80C120,96,240,128,360,138.7C480,149,600,139,720,149.3C840,160,960,192,1080,176C1200,160,1320,96,1380,64L1440,32L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"}
+            customStyles={styles.svgCurve}
+          />
+            <View style={styles.container}>
+              <Image source={logo} style={{height: 120, width: 120, marginTop: 100}} />
+
+              <View style={{width: "60%", alignItems: "center"}}>
+                <Text style={[styles.textFont, {color: variables.alezan, textAlign: "center", marginBottom: 15, fontSize: 12}]}>MOINS DE CHARGE MENTALE, PLUS DE MOMENTS INESTIMABLES !</Text>
+                <TouchableOpacity onPress={()=>navigation.navigate("Login")} style={[styles.button, {justifyContent: "flex-end"}]}>
+                    <Text style={[styles.text, styles.textFont]}>Vivre l'aventure</Text>
+                </TouchableOpacity>
+              </View>
             </View>
         </>
     );
 }
 
 const styles = StyleSheet.create({
-      partieInf: {
+      svgCurve: {
+        position: 'absolute',
+        bottom: 0,
+        width: Dimensions.get('window').width
+      },
+      container: {
         display: "flex",
         flexDirection: "column",
         height: "100%",
-        justifyContent: "flex-end",
-        alignItems: "center"
+        alignItems: "center",
+        justifyContent: "space-between"
       },
       image: {
         flex: 1,
@@ -33,19 +51,23 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         backgroundColor: variables.fond
       },
+      textFont:{
+        fontFamily: "Quicksand"
+      },
       text: {
-        color: "black",
+        color: variables.alezan,
         textAlign: "center",
         fontSize: 16
       },
       button: {
         backgroundColor: "white",
-        paddingLeft: 70,
-        paddingRight: 70,
+        width: "100%",
+        paddingLeft: 20,
+        paddingRight: 20,
         paddingBottom: 15,
         paddingTop: 15,
         borderRadius: 50,
-        marginBottom: 30
+        marginBottom: 50
       }
     
 });
