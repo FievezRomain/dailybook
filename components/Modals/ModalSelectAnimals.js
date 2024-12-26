43f5229f8af45482031aa2db8bd83b62af42b5c4
Romain Fievez <romain.fievezjr@gmail.com>
2024-12-26 13:21:03 +0100

    Correction de la possibilité de scroll horizontal dans une modal pour le système Android

--- components/Modals/ModalSelectAnimals.js
+++ components/Modals/ModalSelectAnimals.js
@@ -86,17 +86,21 @@ const ModalAnimals = ({ modalVisible, setModalVisible, setAnimaux, animaux, sele
       <ModalEditGeneric
         isVisible={modalVisible}
         setVisible={setModalVisible}
+        scrollInside={false}
         arrayHeight={["25%"]}
       >
+        <View style={{paddingVertical: 5}}>
           <AnimalsPicker
-              setAnimaux={setAnimaux}
-              animaux={animaux}
-              mode="multiple"
-              selected={selected}
-              setSelected={setSelected}
-              setValue={setValue}
-              valueName={valueName}
+            setAnimaux={setAnimaux}
+            animaux={animaux}
+            mode="multiple"
+            selected={selected}
+            setSelected={setSelected}
+            setValue={setValue}
+            valueName={valueName}
           />
+        </View>
+          
           <View style={styles.buttonContainer}>
               <Button
               disabled={false}
