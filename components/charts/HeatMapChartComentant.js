e116306bad11edb72684dbee5ba096930a5c0250
Romain Fievez <romain.fievezjr@gmail.com>
2025-02-09 16:08:37 +0100

    Modification de la statistique d'entrainement pour permettre un affichage lisible sur l'affichage mensuel et annuel

--- components/charts/HeatMapChartComentant.js
+++ components/charts/HeatMapChartComentant.js
@@ -1,31 +1,78 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ContributionGraph } from 'react-native-chart-kit';
-import { Dimensions } from 'react-native';
+import { Dimensions, ScrollView } from 'react-native';
 
 const screenWidth = Dimensions.get('window').width;
 
-const HeatMapChartComponent = ({ data, chartConfig, dateFin }) => {
+const HeatMapChartComponent = ({ data, chartConfig, dateDebut, dateFin, handleDayPress }) => {
+
+  const calculateDaysBetweenDates = (date1, date2) => {
+    // Convertir les chaînes de caractères en objets Date
+    const d1 = new Date(date1);
+    const d2 = new Date(date2);
+  
+    // Normaliser les dates à minuit
+    d1.setHours(0, 0, 0, 0);
+    d2.setHours(0, 0, 0, 0);
+  
+    // Calculer la différence en millisecondes
+    const diffInMs = Math.abs(d2 - d1);
+  
+    // Convertir les millisecondes en jours
+    const diffInDays = diffInMs / (1000 * 60 * 60 * 24);
+  
+    return diffInDays;
+  };
+
+  const numDays = calculateDaysBetweenDates(dateDebut, dateFin) + 1;
+  const squareSize = numDays > 32 ? 20 : 40;
+  const horizontal = numDays > 32;
   
   return(
-    <ContributionGraph
-        values={data}
-        endDate={dateFin}
-        numDays={31}
-        width={screenWidth}
-        height={220}
-        chartConfig={chartConfig}
-        squareSize={40}
-        accessor='count'
-        showMonthLabels={true}
-        horizontal={false}
-        onDayPress={() => console.log("uwu")}
-        getMonthLabel={(monthIndex) => {
-            switch(monthIndex){
-                case 0:
-                    return "Janvier";
-            }
-        }}
-    />
+    <ScrollView horizontal={true}>
+      <ContributionGraph
+          values={data}
+          endDate={dateFin}
+          numDays={numDays}
+          width={horizontal ? 1200 : screenWidth}
+          height={220}
+          chartConfig={chartConfig}
+          squareSize={squareSize}
+          accessor='count'
+          
+          showMonthLabels={horizontal}
+          horizontal={horizontal}
+          onDayPress={(date) => handleDayPress(date)}
+          getMonthLabel={(monthIndex) => {
+              switch(monthIndex){
+                  case 0:
+                      return "Janvier";
+                  case 1:
+                      return "Février";
+                  case 2:
+                      return "Mars";
+                  case 3:
+                      return "Avril";
+                  case 4:
+                      return "Mai";
+                  case 5: 
+                      return "Juin";
+                  case 6:
+                      return "Juillet";
+                  case 7:
+                      return "Août";
+                  case 8:
+                      return "Septembre";
+                  case 9:
+                      return "Octobre";
+                  case 10:
+                      return "Novembre";
+                  case 11:
+                      return "Décembre";
+              }
+          }}
+      />
+    </ScrollView>
   )
 };
 
--- components/statistics/EntrainementComponent.js
+++ components/statistics/EntrainementComponent.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { ActivityIndicator, View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { ActivityIndicator, View, Text, StyleSheet, FlatList, TouchableOpacity, SectionList } from 'react-native';
 import { IconButton, useTheme } from 'react-native-paper';
 import EventCard from '../cards/EventCard';
 import Toast from "react-native-toast-message";
@@ -9,15 +9,78 @@ const EntrainementComponent = ({ data, chartConfig, chartParameters }) => {
     const { colors, fonts } = useTheme();
     const [loading, setLoading] = useState(true);
     const [dataToDisplay, setDataToDisplay] = useState(data);
+    const [dataByDate, setDataByDate] = useState(null);
+    const flatListRef = useRef(null);
+    const [expandedDate, setExpandedDate] = useState(null);
 
     useEffect(() => {
         setLoading(true);
 
+        setDataByDate(groupEvents(data, chartParameters.dateDebut, chartParameters.dateFin));
         setDataToDisplay(data);
-        console.log(data)
 
         setLoading(false);
     }, [data]);
+
+    const groupEvents = (data, dateDebut, dateFin) => {
+        const monthDifference = calculateMonthDifference(dateDebut, dateFin);
+
+        if( monthDifference === 0 ){ // Si affichage par mois, on affiche par date
+            return groupByDay(data);
+        } else { // Si affichage par an, on affiche par mois
+            return groupByMonth(data);
+        }
+    }
+
+    const groupByDay = (data) => {
+        const map = new Map();
+
+        data.statistic.forEach((stat) => {
+            if (!map.has(stat.date)) {
+                map.set(stat.date, { date: stat.date, events: [...stat.events] });
+            }
+        });
+
+        return Array.from(map.values());
+    }
+
+    const groupByMonth = (data) => {
+        const map = new Map();
+        const processedDates = new Set();
+
+        data.statistic.forEach((stat) => {
+            // Vérifie si cette date a déjà été traitée
+            if (processedDates.has(stat.date)) {
+                return; // Passe à l'élément suivant si la date a déjà été traitée
+            }
+        
+            // Ajoute la date au Set des dates traitées
+            processedDates.add(stat.date);
+
+            // Extraire l'année et le mois de la date
+            const statDate = new Date(stat.date);
+            const yearMonth = `${statDate.getFullYear()}-${String(statDate.getMonth() + 1).padStart(2, '0')}`; // Format "YYYY-MM"
+        
+            // Si la clé "yearMonth" n'existe pas, on l'ajoute
+            if (!map.has(yearMonth)) {
+                map.set(yearMonth, { date: yearMonth, events: [...stat.events] }); // On ajoute tout le tableau d'événements
+              } else {
+                // Sinon, on fusionne les nouveaux événements avec ceux existants
+                const existingEvents = map.get(yearMonth).events;
+                map.get(yearMonth).events = [...existingEvents, ...stat.events];
+              }
+        });
+
+        return Array.from(map.values());
+    }
+
+    const calculateMonthDifference = (dateDebut, dateFin) => {
+        // Vérification si par mois ou par an
+        var [jourDebut, moisDebut, anneeDebut] = dateDebut.split("/").map(Number);
+        var [jourFin, moisFin, anneeFin] = dateFin.split("/").map(Number);
+
+        return moisFin - moisDebut;
+    }
 
     const handleEventsChange = async () => {
 
@@ -35,6 +98,60 @@ const EntrainementComponent = ({ data, chartConfig, chartParameters }) => {
             return new Date(annee, mois - 1, jour);
         }
     }
+
+    const getDateDebut = ( ) => {
+        if(chartParameters.dateFin){
+            var [jour, mois, annee] = chartParameters.dateDebut.split("/").map(Number);
+            return new Date(annee, mois - 1, jour);
+        }
+    }
+
+    const handleDayPress = (day) => {
+        if(day.events === undefined){
+            return;
+        }
+        // Trouve l'index de la date dans la liste
+        const monthDifference = calculateMonthDifference(chartParameters.dateDebut, chartParameters.dateFin);
+        let date = null;
+        if(monthDifference > 0){
+            date = new Date(day.date);
+            date = `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}`;
+        } else{
+            date = day.date;
+        }
+        const index = dataByDate.findIndex((item) => item.date === date);
+    
+        if (index !== -1) {
+            setExpandedDate(date);
+            // Fait défiler la FlatList vers cet index
+            flatListRef.current.scrollToIndex({ index, animated: true });
+        }
+    };
+
+    const handleDateCategoryPress = (date) => {
+        // Si la date est déjà ouverte, on la referme, sinon on l'ouvre
+        setExpandedDate(expandedDate === date ? null : date);
+    };
+
+    const getDateToDisplay = (date) => {
+        const monthDifference = calculateMonthDifference(chartParameters.dateDebut, chartParameters.dateFin);
+        const options = monthDifference > 0 ? { month: 'long', year: 'numeric' } : { day: '2-digit', month: 'long', year: 'numeric' };
+        const formatter = new Intl.DateTimeFormat('fr-FR', options);
+
+        let dateFormatted = formatter.format( new Date(date) );
+
+        // Si monthDifference > 0, mettre la première lettre du mois en majuscule
+        if (monthDifference > 0) {
+            dateFormatted = dateFormatted.charAt(0).toUpperCase() + dateFormatted.slice(1);
+        }
+
+        return `${dateFormatted}`;
+    }
+
+    const isExpanded = (item) => {
+        return expandedDate === item.date;
+    }
+    
 
     const styles = StyleSheet.create({
         container:{
@@ -86,32 +203,50 @@ const EntrainementComponent = ({ data, chartConfig, chartParameters }) => {
 
     return(
         <>
-            <View style={styles.container}>
-                <Text style={styles.text}>Total:  €</Text>
-            </View>
             <HeatMapChartComponent
                 chartConfig={chartConfig}
                 data={dataToDisplay.statistic}
+                dateDebut={getDateDebut()}
                 dateFin={getDateFin()}
+                handleDayPress={handleDayPress}
             />
             
             <FlatList
-                data={dataToDisplay.statistic}
+                ref={flatListRef}
+                data={dataByDate}
                 keyExtractor={(item, index) => index}
                 renderItem={({ item }) => (
                     <>
-                        <TouchableOpacity style={styles.card}>
+                        <TouchableOpacity style={styles.card} onPress={() => handleDateCategoryPress(item.date)}>
                             <View style={styles.cardContainer}>
                                 <View style={styles.categorieContainer}>
                                     <View style={[styles.indicatorCategorie, {backgroundColor: item.color, marginRight: 15}]} />
-                                    <Text>{item.date}</Text>
+                                    <Text>{getDateToDisplay(item.date)}</Text>
                                 </View>
                                 <View style={styles.categorieContainer}>
-                                    <Text>{item.exact_value} €</Text>
+                                {expandedDate === item.date ?
+                                    <IconButton icon={"chevron-up"} size={20} />
+                                :
+                                    <IconButton icon={"chevron-down"} size={20} />
+                                }
                                 </View>
                                 
                             </View>
                         </TouchableOpacity>
+
+                        {(isExpanded(item)) && (
+                            <FlatList
+                                data={item.events}
+                                keyExtractor={(event) => event.id.toString()}
+                                renderItem={({ item }) => (
+                                    <EventCard
+                                        eventInfos={item}
+                                        withDate={ true }
+                                        handleEventsChange={handleEventsChange}
+                                    />
+                                )}
+                            />
+                        )}
                     </>
                 )}
                 scrollEnabled={false}
