ccd6059ddc63f583a4637f701a78db4998149ea6
Romain Fievez <romain.fievezjr@gmail.com>
2025-04-04 14:36:38 +0200

    Mise en place de secrets pour la configuration aws

---
+++ aws-exports.js
@@ -0,0 +1,9 @@
+const awsconfig = {
+    region: process.env.AWS_REGION,
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID, 
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY 
+    }
+  };
+  
+  export default awsconfig;
