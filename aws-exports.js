1eb9a7c2745493878c6a5d1275a0d00cb242687c
Romain Fievez <romain.fievezjr@gmail.com>
2025-04-06 12:34:37 +0200

    suppression du fichier aws-exports

--- aws-exports.js
+++
@@ -1,9 +0,0 @@
-const awsconfig = {
-    region: process.env.EXPO_PUBLIC_AWS_REGION,
-    credentials: {
-        accessKeyId: process.env.EXPO_PUBLIC_AWS_ACCESS_KEY_ID, 
-        secretAccessKey: process.env.EXPO_PUBLIC_AWS_SECRET_ACCESS_KEY 
-    }
-  };
-  
-  export default awsconfig;
