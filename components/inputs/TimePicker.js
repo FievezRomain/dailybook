85acd59cd096e11f55dacaf415b90720784720c4
Romain Fievez <romain.fievezjr@gmail.com>
2025-07-05 21:18:37 +0200

    Ajout d'un icon de croix pour supprimer l'heure saisie

--- components/inputs/TimePicker.js
+++ components/inputs/TimePicker.js
@@ -3,6 +3,7 @@ import { View, Button, Text } from 'react-native';
 import DateTimePickerModal from "react-native-modal-datetime-picker";
 import { TouchableOpacity } from 'react-native';
 import { useTheme } from 'react-native-paper';
+import { Entypo } from '@expo/vector-icons';
 
 const TimePickerCustom = ({setValue, valueName, defaultValue}) => {
   const { colors, fonts } = useTheme();
@@ -27,7 +28,13 @@ const TimePickerCustom = ({setValue, valueName, defaultValue}) => {
     <>
       <TouchableOpacity onPress={showDatePicker} style={{backgroundColor: colors.quaternary, alignSelf: "flex-start", padding: 10, borderRadius: 5, width: "100%"}}>
         {selectedDate ?
+          <View style={{flexDirection: "row", alignItems: "center", justifyContent: "space-between"}}>
             <Text style={{fontFamily: fonts.default.fontFamily}}>{String(selectedDate.getHours()).padStart(2, '0')}h{String(selectedDate.getMinutes()).padStart(2, '0')}</Text>
+            <TouchableOpacity onPress={() => {setValue(valueName, undefined); setSelectedDate(null);}}>
+              <Entypo name="circle-with-cross" size={15} color={colors.default_dark}/>
+            </TouchableOpacity>
+          </View>
+            
           :
             <Text style={[{fontFamily: fonts.default.fontFamily}, {color: colors.secondary}]}>Sélectionner une heure</Text>
         }
