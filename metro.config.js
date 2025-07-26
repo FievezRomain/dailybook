85c8926b67e7c5c2fee2e11c871ab9fb26c7f636
Romain Fievez <romain.fievezjr@gmail.com>
2025-07-26 15:32:50 +0200

    Ajout d'une propriété dans app.json et renommage d'une constante dans la config metro

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,8 @@
 const { getDefaultConfig } = require('@expo/metro-config');
 
-const defaultConfig = getDefaultConfig(__dirname);
-defaultConfig.resolver.assetExts.push('cjs');
+const config = getDefaultConfig(__dirname);
+config.resolver.assetExts.push('cjs');
 
-defaultConfig.resolver.unstable_enablePackageExports = false;
+config.resolver.unstable_enablePackageExports = false;
 
-module.exports = defaultConfig;
+module.exports = config;
